5d7fd39e92733ffb6a24f9bfaeec70d68b9cae90
enzo <enzo1998garcia@gmail.com>
2023-08-07 19:44:37 -0300

    mejoras

--- app/components/LoginForm.js
+++ app/components/LoginForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, Text, TouchableOpacity,ActivityIndicator  } from 'react-native';
 import { isValidObjectField, updateError } from '../utils/methods';
 import FormContainer from './FormContainer';
 import FormInput from './FormInput';
@@ -13,52 +13,62 @@ import { useUserContext } from '../../UserContext';
 const LoginForm = () => {
   const navigation = useNavigation(); // Obtiene el objeto de navegación
   const { updateUser } = useUserContext();
+  const [isLoading, setIsLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [isConnected, setIsConnected] = useState(true); // Estado conexión
+  const [error, setError] = useState('');
+ 
 
   const [userInfo, setUserInfo] = useState({
     usuario: '',
     contrasenia: '',
   });
 
-  const [showPassword, setShowPassword] = useState(false);
+  const { usuario, contrasenia } = userInfo;
 
   const togglePasswordVisibility = () => {
     setShowPassword((prevState) => !prevState);
   };
-
-  const [error, setError] = useState('');
-
-  const { usuario, contrasenia } = userInfo;
-
- 
 
   const handleOnChangeText = (value, fieldName) => {
     setUserInfo({ ...userInfo, [fieldName]: value });
   };
 
   const submitForm = async () => {
-      try {
-        const res = await client.post('/empleados/logueoChofer', { ...userInfo });
-        console.log(res.data)
-        if (res.data && res.data.length > 0 && res.data[0].Tipo === 'C') {
-          updateUser(res.data[0].usuarioC);
-          setUserInfo({ usuario: '', contrasenia: '' });
-          navigation.navigate('MenuForm', { user: res.data[0] }); // Navega a la pantalla MenuForm
-        } else {
-          updateError('Usuario y/o Contraseña incorrectos', setError);
-        }
-      } catch (error) {
-        updateError('Error al conectarce con el Servidor:', setError);
+    try {
+      setIsLoading(true);
+      const res = await client.post('/empleados/logueoChofer', { ...userInfo });
+      console.log(res.data);
+      if (res.data && res.data.length > 0 && res.data[0].Tipo === 'C') {
+        updateUser(res.data[0].usuarioC);
+        setUserInfo({ usuario: '', contrasenia: '' });
+        setIsConnected(true);
+        navigation.navigate('MenuForm', { user: res.data[0] }); // Navega a la pantalla MenuForm
+      } else {
+        setIsConnected(true);
+        console.log('en catch');
+        updateError('Usuario y/o Contraseña incorrectos', setError);
       }
-    
+    } catch (error) {
+      setIsConnected(false);
+      console.log('en catch');
+      updateError('Error al conectarce con el Servidor: verificar su conexión a Internet', setError);
+    } finally {
+      setIsLoading(false); // Desactiva la rueda de carga después de completar la tarea
+    }
   };
 
   return (
-    
     <FormContainer>
-        <View style={{ height: 180,paddingTop: 100 }}>
-          <FromHeader centerHeading='Bienvenido' subHeading='Transporte FED' />
-        </View>
-        {error ? (<Text style={{ color: 'red', fontSize: 18, textAlign: 'center', height: 40,paddingTop: 5 }}>{error}</Text>) : null}
+      <View style={{ height: 180, paddingTop: 100 }}>
+        <FromHeader centerHeading='Bienvenido' subHeading='Transporte FED' />
+      </View>
+      {error ? (
+        <Text style={{ color: 'red', fontSize: 18, textAlign: 'center', height: 45 }}>
+          {error}
+        </Text>
+      ) : null}
+
       <FormInput
         value={usuario}
         onChangeText={(value) => handleOnChangeText(value, 'usuario')}
@@ -71,19 +81,24 @@ const LoginForm = () => {
         value={contrasenia}
         onChangeText={(value) => handleOnChangeText(value, 'contrasenia')}
         autoCapitalize='none'
-        secureTextEntry={!showPassword} 
+        secureTextEntry={!showPassword}
         label='Contraseña'
         placeholder='**********'
       />
       <TouchableOpacity onPress={togglePasswordVisibility}>
-      <Icon
-        name={showPassword ? 'eye-slash' : 'eye'}
-        size={24}
-        color="#000"
-        style={{ position: 'absolute', right: 15, top: -50, color:"#c1c1c1" }}
-         />
+        <Icon
+          name={showPassword ? 'eye-slash' : 'eye'}
+          size={24}
+          color="#000"
+          style={{ position: 'absolute', right: 15, top: -50, color: "#c1c1c1" }}
+        />
       </TouchableOpacity>
-      <FormSubmitButton onPress={submitForm} titulo='iniciar sesión ' />
+      
+      {isLoading ? (
+        <ActivityIndicator size="large" color="#0000ff" style={{ marginTop: 20 }} />
+      ) : (
+        <FormSubmitButton onPress={submitForm} titulo='iniciar sesión ' />
+      )}
     </FormContainer>
   );
 };
--- app/utils/methods.js
+++ app/utils/methods.js
@@ -8,8 +8,8 @@ export const isValidObjectField = (obj) =>{
     return Object.values(obj).every(value => value.trim())
    }
   
-export const updateError = (error, updateError) =>{
-    updateError(error);
+export const updateError = (errorMessage, setErrorState) =>{
+  setErrorState(errorMessage);
    }
 
    export const loadImageFromGallery = async () => {
